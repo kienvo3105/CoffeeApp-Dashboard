9b95425f822e8b44d29ccea6068f21ffcfaea9c9
kienvo3105 <hvtrungkien@gmail.com>
2022-12-09 14:45:59 +0700

    update showtime

--- src/Pages/showtime/AddShowTime.jsx
+++ src/Pages/showtime/AddShowTime.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { Button, Form, Input, notification, Select } from "antd";
+import { Button, Form, Input, notification, Select, DatePicker, TimePicker } from "antd";
 
 import { usePost, useGet } from "../../api";
 import { Layout } from "../../Layout/Layout";
 
 export const AddShowTime = () => {
+    const dateFormat = "DD/MM/YYYY";
 
     const { fetchGet, result: Optionsresult } = useGet();
     const { fetchGet: fetchgetProvince, result: OptionsresultCinema } = useGet();
+    const { fetchGet: fetchgetMovie, result: OptionsresultMovie } = useGet();
+    const { fetchGet: fetchgetRoom, result: OptionsresultRoom } = useGet();
     const [options, setOptions] = React.useState(undefined);
     const [optionsCinema, setOptionsCinema] = React.useState(undefined);
+    const [optionsMovie, setOptionsMovie] = React.useState(undefined);
+    const [optionsRoom, setOptionsRoom] = React.useState(undefined);
     const { fetchPost, isLoading, result } = usePost();
     const openNotificationWithIcon = (type, message = "", des = "") => {
         notification[type]({
@@ -18,15 +23,44 @@ export const AddShowTime = () => {
         });
     };
     const onFinish = (values) => {
-        console.log();
+        //console.log(values.Date._d.toISOString());
+        console.log(values.Time._d.getHours() + ":" + values.Time._d.getMinutes());
+        console.log({
+            ...values,
+            date: values.Date._d.toISOString(),
+            time: values.Time._d.getHours() + ":" + values.Time._d.getMinutes()
+        });
         fetchPost("showtime", {
             ...values,
+            date: values.Date._d.toISOString(),
+            time: values.Time._d.getHours() + ":" + values.Time._d.getMinutes()
         });
     };
 
     React.useEffect(() => {
         openNotificationWithIcon("success", "Thêm xuất chiếu mới thành công");
     }, [result]);
+
+    React.useEffect(() => {
+        fetchgetMovie("movie");
+        // eslint-disable-next-line
+    }, [])
+    React.useEffect(() => {
+        if (OptionsresultMovie) {
+            const newOptions = OptionsresultMovie?.map((option) => {
+                return {
+                    label: option.name,
+                    value: option._id,
+
+                };
+            });
+            setOptionsMovie(newOptions);
+
+
+        }
+        // eslint-disable-next-line
+    }, [OptionsresultMovie]);
+
 
     React.useEffect(() => {
         fetchGet("province");
@@ -62,12 +96,34 @@ export const AddShowTime = () => {
                     value: option._id,
                 };
             });
-            console.log(cinema);
-            console.log(OptionsresultCinema);
+
             setOptionsCinema(cinema);
         }
         // eslint-disable-next-line
     }, [OptionsresultCinema]);
+
+
+    const fetchRoom = (id) => {
+        fetchgetRoom("cinema/" + id);
+    }
+
+
+    React.useEffect(() => {
+        if (OptionsresultRoom) {
+
+            //cinema
+            const room = OptionsresultRoom?.rooms?.map((option) => {
+                return {
+                    label: option.name,
+                    value: option._id,
+                };
+            });
+            console.log((room));
+            console.log(OptionsresultRoom.rooms);
+            setOptionsRoom(room);
+        }
+        // eslint-disable-next-line
+    }, [OptionsresultRoom]);
 
     return (
         <Layout>
@@ -83,6 +139,27 @@ export const AddShowTime = () => {
                         autoComplete="off"
                     >
                         <Form.Item
+                            label="Phim"
+                            name="movieId"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Vui lòng chọn tên phim",
+                                },
+                            ]}
+                        >
+                            <Select
+                                placeholder="Chọn tên phim"
+                                filterOption={(input, option) =>
+                                    (option?.label ?? "")
+                                        .toLowerCase()
+                                        .includes(input.toLowerCase())
+                                }
+                                options={optionsMovie}
+                            />
+                        </Form.Item>
+
+                        <Form.Item
                             label="Ngày chiếu:"
                             name="Date"
                             rules={[
@@ -92,7 +169,7 @@ export const AddShowTime = () => {
                                 },
                             ]}
                         >
-                            <Input type="Date" />
+                            <DatePicker format={dateFormat} />
                         </Form.Item>
 
                         <Form.Item
@@ -137,7 +214,35 @@ export const AddShowTime = () => {
                                         .toLowerCase()
                                         .includes(input.toLowerCase())
                                 }
+                                onChange={(e) => {
+                                    console.log(e);
+                                    fetchRoom(e);
+
+
+                                }}
                                 options={optionsCinema}
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Phòng"
+                            name="roomId"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "Vui lòng chọn phòng",
+                                },
+                            ]}
+                        >
+                            <Select
+                                placeholder="Chọn Phòng"
+                                filterOption={(input, option) =>
+                                    (option?.label ?? "")
+                                        .toLowerCase()
+                                        .includes(input.toLowerCase())
+                                }
+
+                                options={optionsRoom}
                             />
                         </Form.Item>
 
@@ -151,7 +256,7 @@ export const AddShowTime = () => {
                                 },
                             ]}
                         >
-                            <Input type="Time" />
+                            <TimePicker format="HH:mm"></TimePicker>
                         </Form.Item>
 
                         <Form.Item wrapperCol={{ offset: 6, span: 18 }}>
--- src/Pages/showtime/ShowTime.jsx
+++ src/Pages/showtime/ShowTime.jsx
@@ -1,10 +1,114 @@
-import { Breadcrumb, Button } from "antd";
-import React from "react";
+
 import { useNavigate } from "react-router-dom";
 import { Layout } from "../../Layout/Layout";
 
+import React from "react";
+import { Button, Form, Input, notification, Select, Breadcrumb } from "antd";
+
+import { useGet } from "../../api";
+const Dates = [];
+for (let i = 0; i <= 6; i++) {
+  const toDate = new Date();
+  const temp = new Date();
+  temp.setDate(toDate.getDate() + i);
+  const temp2 =
+    (temp.getDay() !== 0 ? "T" + (1 + temp.getDay()) : "CN") +
+    ", " +
+    temp.getDate() +
+    "/" +
+    (temp.getMonth() + 1);
+  temp.setHours(7);
+  temp.setMinutes(0);
+  temp.setSeconds(1)
+  Dates.push({
+    time: temp2,
+    id: i + 1,
+    date: temp.toISOString()
+  });
+}
+
+
 export const ShowTime = () => {
   let navigate = useNavigate();
+
+  let getIdCity = 1;
+  let getIdDate = 1;
+
+  const [ShowTime, setShowTime] = React.useState([
+    {
+      name: "Hà Nội 1",
+      id: 1,
+      idCity: 1,
+      idDate: 1,
+      time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
+    },
+    {
+      name: "Hà Nội 2",
+      id: 4,
+      idCity: 1,
+      idDate: 1,
+      time: ["9h30 AM", "10h AM", "11h30 AM", "1h PM"],
+    },
+  ]);
+
+  const [date, setDate] = React.useState(Dates[0].date);
+
+
+
+  const [clickedCity, getclickedCity] = React.useState(1);
+  const getShowtime = (id) => {
+    getIdCity = id;
+    getclickedCity(id);
+    console.log("getShowTime " + id);
+
+  };
+
+
+  const { fetchGet, result: Optionsresult } = useGet();
+  const { fetchGet: fetchgetProvince, result: OptionsresultCinema } = useGet();
+  const { fetchGet: fetchGetShowtime, result: showtimeResult } = useGet();
+  const [optionsCinema, setOptionsCinema] = React.useState(undefined);
+
+
+
+
+
+  React.useEffect(() => {
+    fetchGet("province");
+    // eslint-disable-next-line
+  }, [])
+
+  const fetchProvince = (id) => {
+    fetchgetProvince("province/" + id)
+  }
+  const fetchShowtime = (id) => {
+    fetchGetShowtime("showtime/")
+  }
+
+  React.useEffect(() => {
+    if (OptionsresultCinema) {
+
+      //cinema
+      const cinema = OptionsresultCinema?.cinemas
+
+      setOptionsCinema(cinema);
+    }
+    // eslint-disable-next-line
+  }, [OptionsresultCinema]);
+
+
+
+
+
+
+  React.useEffect(() => {
+    if (showtimeResult) {
+      console.log(showtimeResult)
+    }
+    // eslint-disable-next-line
+  }, [showtimeResult]);
+
+
   return (
     <Layout>
       <Breadcrumb style={{ marginLeft: "16px" }}>
@@ -14,7 +118,59 @@ export const ShowTime = () => {
         <Button onClick={() => navigate("/showtime/add")} type="primary" danger>
           Add ShowTime
         </Button>
+        <div className="p-[24px] min-h-[360px] bg-white my-[50px] mx-[200px]">
+          <div className="border-y-4 border-black py-5">
+            {Dates.map((item) => (
+              <button
+                onClick={() => setDate(item.date)}
+                type="button"
+                key={item.id}
+                className="ml-5 border bg-sky-700 hover:bg-sky-300 text-white h-[50px] w-[100px] rounded-xl"
+
+              >
+                {item.time}
+              </button>
+            ))}
+          </div>
+
+          <div className="py-5">
+            {Optionsresult && Optionsresult.map((item) => (
+              <button
+                onClick={() => fetchProvince(item._id)}
+                type="button"
+                key={item._id}
+                className="ml-5 border hover:bg-sky-300  text-white h-[50px] w-[100px] rounded-xl"
+                style={{
+                  backgroundColor: clickedCity === item._id ? "#0288D1" : "gray",
+                }}
+              >
+                {item.name}
+              </button>
+            ))}
+          </div>
+          <div>
+            {optionsCinema && optionsCinema.map((item) => (
+              <div className="border-t-2 border-slate-600 py-5 mx-[50px]">
+                <div onClick={() => {
+                  fetchShowtime()
+                }} className="text-[30px] mb-[20px]">{item.name}</div>
+                {/* {item.time.map((temp, index) => (
+                  <button
+                    
+                    type="button"
+                    key={index}
+                    className="ml-5 border bg-gray-700 hover:bg-sky-300 text-white h-[50px] w-[100px]"
+                  >
+                    {temp}
+                  </button>
+                ))} */}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+
+
     </Layout>
   );
 };
